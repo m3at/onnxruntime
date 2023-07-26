8ea74d24b66379a6a93a1ae945a84fdd098d1e7e
Changming Sun <chasun@microsoft.com>
2023-07-26 03:05:46 +0000

    update

--- tools/ci_build/github/linux/docker/scripts/install_protobuf.sh
+++ tools/ci_build/github/linux/docker/scripts/install_protobuf.sh
@@ -39,13 +39,25 @@ case "$(uname -s)" in
       exit -1
 esac
 mkdir -p $INSTALL_PREFIX
+
+echo "Installing abseil ..."
+protobuf_url=$(grep '^abseil_cpp' $DEP_FILE_PATH | cut -d ';' -f 2)
+curl -sSL --retry 5 --retry-delay 10 --create-dirs --fail -L -o abseil.zip $protobuf_url
+mkdir abseil
+cd abseil
+unzip ../abseil.zip
+cd *
+cmake "."  "-DABSL_PROPAGATE_CXX_STD=ON" "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_TESTING=OFF" "-DABSL_USE_EXTERNAL_GOOGLETEST=ON" "-DCMAKE_PREFIX_PATH=$install_prefix" "-DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX" -G Ninja
+ninja
+ninja install
+
 echo "Installing protobuf ..."
 protobuf_url=$(grep '^protobuf' $DEP_FILE_PATH | cut -d ';' -f 2 | sed 's/\.zip$/\.tar.gz/')
 curl -sSL --retry 5 --retry-delay 10 --create-dirs --fail -L -o protobuf_src.tar.gz $protobuf_url
 mkdir protobuf
 cd protobuf
 tar -zxf ../protobuf_src.tar.gz --strip=1
-cmake ./cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF $EXTRA_CMAKE_ARGS
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
+cmake . -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX $EXTRA_CMAKE_ARGS  -G Ninja
+ninja
+ninja install
 cd ..
